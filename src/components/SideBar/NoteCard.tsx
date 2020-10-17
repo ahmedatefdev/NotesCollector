48d46758fe79eaedee94384403e3e532d489a244
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2020-10-17 19:20:51 +0200

    note content functionality Done

--- src/components/SideBar/NoteCard.tsx
+++ src/components/SideBar/NoteCard.tsx
@@ -18,10 +18,13 @@ interface Props {
 
 const NoteCard = (props: Props) => {
     const { title, body, order } = props.note
-    const NoteF = useContext(NoteFunctions)
+    const noteFunctions = useContext(NoteFunctions)
 
     return (
-        <div className="note_card">
+        <div className="note_card" onClick={(e) => {
+            noteFunctions?.ChangeCurrentNote(props.note)
+        }
+        }>
             {order}
             <p className="note_card-title">
                 <strong >{title}</strong>
@@ -29,13 +32,18 @@ const NoteCard = (props: Props) => {
             {/* <h2 className="note_card-title">{title}</h2> */}
             <p className="note_card-body">{body.length > 80 ? body.substring(0, 80) + "..." : body}</p>
             <div className="note_card-buttons">
-
-                <button className="note-delete btn btn-warning" onClick={(e) =>
-                    NoteF?.DeleteNote(props.note)
+                <button className="note-delete btn btn-warning" onClick={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    noteFunctions?.DeleteNote(props.note)
+                }
                 }>Delete</button>
+
                 <button className="note-rename btn btn-primary" onClick={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
                     const newTitle = window.prompt("enter the new title", title)
-                    NoteF?.RenameNote(props.note, newTitle ? newTitle : title)
+                    noteFunctions?.RenameNote(props.note, newTitle ? newTitle : title)
                 }}>Rename</button>
             </div>
         </div>
--- src/types/Context.ts
+++ src/types/Context.ts
@@ -1,7 +1,8 @@
 import { Note } from "./Note";
 
 export type NoteFunctionsProvider = {
-  EditNoteBody: (newBody: string) => boolean;
+  // EditNoteBody: (title: string | undefined, newBody: string) => boolean;
   DeleteNote: (oldNote: Note) => boolean;
   RenameNote: (oldNote: Note, newTitle: string) => boolean;
+  ChangeCurrentNote: (note: Note) => void;
 };
